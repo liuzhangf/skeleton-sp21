f9b8c9c545a797364fc24112e788d1b298e87e61
liuzhangf <86759403+liuzhangf@users.noreply.github.com>
2026-01-08 15:43:49 +0800

    seventh commit

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -14,7 +14,11 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> sd = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
         String message = "";
+        int cnt = 0;
         while (true) {
+            if (cnt >= 10000){
+                break;
+            }
             int op = random.nextInt(4);
             String lsmessage = "";
             switch (op) {
@@ -23,31 +27,35 @@ public class TestArrayDequeEC {
                     sd.addFirst(a);
                     ad.addFirst(a);
                     lsmessage += "addFirst(" + a + ")\n";
+                    message += lsmessage;
                     break;
                 case 1:
                     int b = random.nextInt(10000);
                     ad.addLast(b);
                     sd.addLast(b);
                     lsmessage += "addLast(" + b + ")\n";
+                    message += lsmessage;
                     break;
                 case 2:
                     if (!sd.isEmpty() && !ad.isEmpty()) {
                         Integer aa = (Integer) sd.removeFirst();
                         Integer bb = (Integer) ad.removeFirst();
-                        lsmessage += "removeFirst\n";
+                        lsmessage += "removeFirst()\n";
+                        message += lsmessage;
                         assertEquals(message, aa, bb);
                     }
                     break;
                 case 3:
                     if (!sd.isEmpty() && !ad.isEmpty()) {
-                        int aa = (int) sd.removeLast();
-                        int bb = (int) ad.removeLast();
-                        lsmessage += "removeLast\n";
+                        Integer aa = (Integer) sd.removeLast();
+                        Integer bb = (Integer) ad.removeLast();
+                        lsmessage += "removeLast()\n";
+                        message += lsmessage;
                         assertEquals(message, aa, bb);
                     }
                     break;
                 }
-                message += lsmessage;
+            cnt ++;
         }
     }
 }
