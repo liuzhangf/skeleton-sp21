c9f29dd1798caacd6c7889eab32ad8b459e3c94b
liuzhangf <86759403+liuzhangf@users.noreply.github.com>
2026-01-13 13:58:01 +0800

    eighth commit

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -1,4 +1,8 @@
 package gitlet;
+import java.io.*;
+import java.util.*;
+import java.io.Serializable;
+import gitlet.Utils;
 
 // TODO: any imports you need here
 
@@ -10,7 +14,8 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *
  *  @author TODO
  */
-public class Commit {
+
+public class Commit implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -18,9 +23,30 @@ public class Commit {
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided one example for `message`.
      */
+    public final String ID;
+    public String parentID1;
+    public String parentID2;
+    private final long timestamp;
+
+    private  Map<String, String> blobid;
 
     /** The message of this Commit. */
     private String message;
+
+    public Commit(long timestamp, String message, String parentID1, String parentID2, String[] BolbID, String[] text) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.parentID1 = parentID1;
+        this.parentID2 = parentID2;
+        matchBolbWithCommit(BolbID, text);
+        this.ID = Utils.sha1(this.message, timestamp, blobid, parentID1, parentID2);
+    }
+
+    public void matchBolbWithCommit (String[] BolbID, String[] text) {
+        for (int i = 0; i < BolbID.length; i++) {
+            blobid.put(BolbID[i], text[i]);
+        }
+    }
 
     /* TODO: fill in the rest of this class. */
 }
--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,5 +1,12 @@
 package gitlet;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+
+
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author TODO
  */
@@ -8,12 +15,19 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) {
+
+    static final File CWD= new File(System.getProperty("user.dir"));
+    static final File Gitlet = new File(CWD, ".gitlet");
+    static final File Objects = new File(CWD, ".objects");
+    static final File Branches = new File(CWD, ".branches");
+    static final File Head = new File(CWD, "HEAD");
+
+    public static void main(String[] args) throws IOException {
         // TODO: what if args is empty?
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                // TODO: handle the `init` command
+                init();
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
@@ -21,4 +35,43 @@ public class Main {
             // TODO: FILL THE REST IN
         }
     }
+
+    /*
+        .gitlet/
+        ├─ 📂 objects/       # 存放【所有Commit对象+所有Blob对象】的地方
+        │  └─ xxxxxx...      # 只有1个文件：初始Commit的哈希文件（文件名=哈希值，内容=序列化的Commit）
+        ├─ 📂 branches/      # 存放【所有分支】的地方 → ✅【重点】多分支的核心存储目录✅
+        │  └─ master         # 只有1个文件：master分支文件（文件名=分支名，文件内容=该分支指向的Commit哈希）
+        └─ 📄 HEAD           # 全局唯一的「当前分支标记文件」（文件内容=字符串，比如：master）
+     */
+
+    public static void init () throws IOException {
+
+        if (Gitlet.exists()) {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+        else {
+
+            Gitlet.mkdir();
+            Objects.mkdir();
+            Branches.mkdir();
+            Head.createNewFile();
+
+            Commit cm = new Commit(0, "initial commit", null, null, null, null);
+            File Object_commit = new File(Objects, cm.ID);
+            Object_commit.createNewFile();
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(Object_commit));
+            out.writeObject(cm);
+            String head =  "master";            // 标记当前是什么工作的branch。
+            Utils.writeContents( Head , head);
+
+            File branches = new File(Branches, "master");
+            branches.createNewFile();
+            Utils.writeContents( Branches , cm.ID);
+        }
+    }
+
+
+
 }
